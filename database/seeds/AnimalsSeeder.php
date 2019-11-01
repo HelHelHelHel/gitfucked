3b7711a228f87387eb232faedd08effcadae6e3c
Sam Judge <samuel.judge.92@gmail.com>
2019-11-01 11:10:47 +0100

    seeded

---
+++ database/seeds/AnimalsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Animal;
+
+class AnimalsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('animals')->truncate();
+
+        $data = json_decode(file_get_contents(storage_path('clients.json'))); // change "clients.json" if your file is called something else
+
+        // dd($data[0]->pets[0]);
+        foreach($data as $client) {
+            foreach($client->pets as $pet){
+                Animal::insert([
+                    'name' => $pet->name,
+                    'breed' => $pet->breed,
+                    'weight' => $pet->weight,
+                    'age' => $pet->age,
+                    'photo_url' => $pet->photo
+                ]);
+                // var_dump($pet);
+            }
+        }
+    }
+}
